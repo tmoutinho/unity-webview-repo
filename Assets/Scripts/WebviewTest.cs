bb6003055788292bca1759b429d5ad71db9a3b25
Tiago Moutinho <tiagoagm@gmail.com>
2023-08-09 23:07:20 +0100

    webview unity

--- Assets/Scripts/WebviewTest.cs
+++ Assets/Scripts/WebviewTest.cs
@@ -1,3 +1,6 @@
+
+using System;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,28 +8,70 @@ using UnityEngine.UI;
 
 public class WebviewTest : MonoBehaviour
 {
-	public string url = "https://www.conductive.ai";
-	private UniWebView webview;
+	public string url = "https://catalyst-web-client.vercel.app/contest/";
+    public bool showToolbar = true;
+    private UniWebView webview;
 
-	public void OpenWebview()
-	{
-		Debug.Log("OpenWebview");
-		webview = gameObject.AddComponent<UniWebView>();
-		webview.Frame = new Rect(0, 0, Screen.width, Screen.height);
-		webview.ReferenceRectTransform = transform as RectTransform;
-		webview.OnPageFinished += (view, statusCode, url) => { webview.Show(); };
-		webview.Load(url);
-	}
-
-	// Start is called before the first frame update
     void Start()
     {
-        
+        InitializeWebview();
+        webview.Load(url);
     }
 
-    // Update is called once per frame
+    private void InitializeWebview()
+    {
+        if (webview == null)
+        {
+            Debug.Log("Initializing Webview");
+            var webviewGO = new GameObject("webviewGO");
+            webview = webviewGO.AddComponent<UniWebView>();
+            webview.Frame = new Rect(0, 0, Screen.width, Screen.height);
+            webview.ReferenceRectTransform = transform as RectTransform;
+            
+            if (showToolbar)
+                webview.EmbeddedToolbar.Show();
+
+            webview.OnPageFinished += (view, statusCode, url) => { Debug.Log("Page Load Finished: " + url); };
+        }
+    }
+
+    public void OpenWebview()
+    {
+        Debug.Log("OpenWebview");
+        webview.Show();
+
+        if (webview == null)
+        {
+            InitializeWebview();
+
+						byte[] bytesToEncode = Encoding.UTF8.GetBytes ("{data:\"name\",unique_identifier:\"aad\"}");
+						string encodedText = Convert.ToBase64String (bytesToEncode);
+
+						webview.Load($"{url}/{encodedText}");
+					}
+    }
+
+    void CloseWebView()
+    {
+        Destroy(webview);
+        webview = null;
+    }
+
+    void OnDestroy()
+    {
+        CloseWebView();
+    }
+
     void Update()
     {
-        
+        if (webview == null)
+        {
+            InitializeWebview();
+            
+						byte[] bytesToEncode = Encoding.UTF8.GetBytes ("{data:\"name\",unique_identifier:\"aad\"}");
+						string encodedText = Convert.ToBase64String (bytesToEncode);
+
+						webview.Load($"{url}/{encodedText}");
+        }
     }
 }
